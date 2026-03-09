9a1c660562433dac21d507bdd7e6b63259ecdbe4
ab_dz <152961090+ab7dz@users.noreply.github.com>
2026-03-09 23:23:54 +0100

    Delete stockfish-worker.js

--- stockfish-worker.js
+++
@@ -1,6 +0,0 @@
-// stockfish-worker.js
-// wrapper بسيط: يحمل stockfish-18-lite ويفعّل وضع Worker تلقائياً
-// عندما يُحمَّل عبر importScripts، الملف يكتشف أنه داخل Worker
-// ويضبط onmessage تلقائياً للتعامل مع أوامر UCI
-
-self.importScripts('stockfish-18-lite.js');
